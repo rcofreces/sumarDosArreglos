266004a6d1126619ada4bd64a377151a01651876
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-19 15:40:25 -0300

    Commit19/05 Arreglé errores del ejercicio. Falta para terminar.

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,25 @@ let arreglo2: number[] = new Array(6);
 let indice1: number;
 let indice2: number;
 
-for (indice1 = 0; indice1 < arreglo1[indice1]; indice1++) {
+for (indice1 = 0; indice1 < 6; indice1++) {
   arreglo1[indice1] = Number(
     prompt(`Ingrese el número del arreglo 1 en la posición ${indice1}`)
   );
 }
 
-for (indice2 = 0; indice2 < arreglo2[indice2]; indice2++) {
+for (indice2 = 0; indice2 < 6; indice2++) {
   arreglo2[indice2] = Number(
     prompt(`Ingrese el número del arreglo 2 en la posición ${indice1}`)
   );
 }
 
-for (indice1 = 0; indice1 < arreglo1[indice1]; indice1++) {
+for (indice1 = 0; indice1 < 6; indice1++) {
   console.log(
     `El número ingresado en la posición ${indice1} del arreglo 1 es: ${arreglo1[indice1]}`
   );
 }
 
-for (indice2 = 0; indice2 < arreglo2[indice2]; indice2++) {
+for (indice2 = 0; indice2 < 6; indice2++) {
   console.log(
     `El número ingresado en la posición ${indice2} del arreglo 2 es: ${arreglo2[indice2]}`
   );
