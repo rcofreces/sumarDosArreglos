15792bca315f57dbb536f58e26456c05c60a1d28
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-05-19 15:52:59 -0300

    Commit19/05 Terminé el ejercicio pero no de la forma más óptima.

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,29 @@ for (indice2 = 0; indice2 < 6; indice2++) {
     `El número ingresado en la posición ${indice2} del arreglo 2 es: ${arreglo2[indice2]}`
   );
 }
+
+let resultado0: number = arreglo1[0] + arreglo2[0];
+let resultado1: number = arreglo1[1] + arreglo2[1];
+let resultado2: number = arreglo1[2] + arreglo2[2];
+let resultado3: number = arreglo1[3] + arreglo2[3];
+let resultado4: number = arreglo1[4] + arreglo2[4];
+let resultado5: number = arreglo1[5] + arreglo2[5];
+
+console.log(
+  `El resultado de la suma de la primera posición de los arreglos 1 y 2 es: ${resultado0}`
+);
+console.log(
+  `El resultado de la suma de la segunda posición de los arreglos 1 y 2 es: ${resultado1}`
+);
+console.log(
+  `El resultado de la suma de la tercer posición de los arreglos 1 y 2 es: ${resultado2}`
+);
+console.log(
+  `El resultado de la suma de la cuarta posición de los arreglos 1 y 2 es: ${resultado3}`
+);
+console.log(
+  `El resultado de la suma de la quinta posición de los arreglos 1 y 2 es: ${resultado4}`
+);
+console.log(
+  `El resultado de la suma de la sexta posición de los arreglos 1 y 2 es: ${resultado5}`
+);
